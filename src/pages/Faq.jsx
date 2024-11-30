f37b5f8577f160bb8a00305488aabacdff58739f
Nkemka Akah <nkemkaomeiza@gmail.com>
2024-11-30 17:17:28 +0000

    fixed abo link

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Faqs4 from '../components/Faqs'
 
 const Faq = () => {
+
+  useEffect(() => {
+    // Scroll to the top of the page when the component is mounted
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className=' bg-white shadow-none rounded-lg pb-10 sm:px-10 px-5 pt-0 border-b-8'>
       {/* <h2 className='text-2xl font-bold text-gray-800 mb-8'>Frequently Asked Questions</h2> */}
